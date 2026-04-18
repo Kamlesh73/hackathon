5213e235f64271625989a054de561554680f1a1f
Kamlesh73 <kamleshjangade@gmail.com>
2026-04-18 04:45:16 -0700

    Create main.tf

---
+++ terraform/modules/eks/main.tf
@@ -0,0 +1,29 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.eks_cluster_name
+  cluster_version = "1.32"
+
+  cluster_endpoint_public_access = true
+  enable_cluster_creator_admin_permissions = true
+
+  eks_managed_node_groups = {
+    example = {
+      instance_types = ["t3.medium"]
+      min_size       = 1
+      max_size       = 5
+      desired_size   = 2
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  tags = {
+    Name        = var.eks_cluster_name
+    Environment = var.environment
+    Terraform   = "true"
+  }
+
+}
